4d58640f8b6d418f3333822f4e0f042b865a9368
mtzyw <528808237@qq.com>
2025-12-22 17:53:20 +0800

    解决报错

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import HomeComponent from "@/components/home";
+import { DEFAULT_LOCALE } from "@/i18n/routing";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <HomeComponent />;
+  redirect(`/${DEFAULT_LOCALE}`);
 }
